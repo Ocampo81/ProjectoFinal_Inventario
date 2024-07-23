f38a6f3035092dcd1c8376ea63916cc7b8895d8f
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-07-23 18:48:53 +0000

    login/signup desing

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,9 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import Login from './pages/Login.jsx'
+import Singup from './pages/Singup.jsx'
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,14 +22,11 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/" />
+                        <Route element={<Singup />} path="/singup" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
---
+++ src/front/js/pages/Singup.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignupPage = () => {
+    return (
+        <section className="text-center text-lg-start">
+            <style>
+                {`.cascading-right {
+          margin-right: -50px;
+        }
+
+        @media (max-width: 991.98px) {
+          .cascading-right {
+            margin-right: 0;
+          }
+        }`}
+            </style>
+
+            <div className="container py-4">
+                <div className="row g-0 align-items-center">
+                    <div className="col-lg-6 mb-5 mb-lg-0">
+                        <div className="card cascading-right bg-body-tertiary"
+                            style={{
+                                backdropFilter: 'blur(30px)',
+                            }}>
+                            <div className="card-body p-5 shadow-5 text-center">
+                                <h2 className="fw-bold mb-5">Sign up now</h2>
+                                <form>
+                                    <div className="row">
+                                        <div className="col-md-6 mb-4">
+                                            <div data-mdb-input-init className="form-outline">
+                                                <input type="text" id="form3Example1" className="form-control" />
+                                                <label className="form-label" htmlFor="form3Example1">First name</label>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 mb-4">
+                                            <div data-mdb-input-init className="form-outline">
+                                                <input type="text" id="form3Example2" className="form-control" />
+                                                <label className="form-label" htmlFor="form3Example2">Last name</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div data-mdb-input-init className="form-outline mb-4">
+                                        <input type="email" id="form3Example3" className="form-control" />
+                                        <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                    </div>
+
+                                    <div data-mdb-input-init className="form-outline mb-4">
+                                        <input type="password" id="form3Example4" className="form-control" />
+                                        <label className="form-label" htmlFor="form3Example4">Password</label>
+                                    </div>
+
+                                    <div className="form-check d-flex justify-content-center mb-4">
+                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+                                        <label className="form-check-label" htmlFor="form2Example33">
+                                            Subscribe to our newsletter
+                                        </label>
+                                    </div>
+
+                                    <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block mb-4">
+                                        Sign up
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-6 mb-5 mb-lg-0">
+                        <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" className="w-100 rounded-4 shadow-4" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default SignupPage;
