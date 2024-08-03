5fed646f6880e08bcaa2dfd020136352ade3c17d
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-08-03 14:23:36 +0000

    Mainmenu

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
 import RegisterCompany from './pages/RegisterCompany.jsx';
+import MainMenu from './pages/MainMenu.jsx'; // Importar el nuevo componente
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,6 +26,7 @@ const Layout = () => {
                         <Route element={<Login />} path="/login" />
                         <Route element={<Signup />} path="/signup" />
                         <Route element={<RegisterCompany />} path="/register-company" />
+                        <Route element={<MainMenu />} path="/main-menu" /> {/* Nueva ruta */}
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                 </ScrollToTop>
